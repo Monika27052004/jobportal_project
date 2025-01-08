67f78df0abe038e8afb68cfe9b156e8366abdf8b
Monika Lalwani <146726090+Monika27052004@users.noreply.github.com>
2025-01-08 09:50:42 +0530

    Update useGetAllCompanies.jsx

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -4,6 +4,35 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
+import { setCompanies } from '@/redux/companySlice';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+const COMPANY_API_END_POINT = "https://jobportal-project-6.onrender.com/api/v1/company";
+
+const useGetAllCompanies = () => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        const fetchCompanies = async () => {
+            try {
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get`, { withCredentials: true });
+                console.log('called');
+                if (res.data.success) {
+                    dispatch(setCompanies(res.data.companies));
+                }
+            } catch (error) {
+                console.error("Error fetching companies:", error);
+            }
+        };
+        fetchCompanies();
+    }, [dispatch]);
+};
+
+export default useGetAllCompanies;
+
+
 const useGetAllCompanies = () => {
     const dispatch = useDispatch();
     useEffect(()=>{
