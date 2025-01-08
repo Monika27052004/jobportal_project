8189d78e5aee22e54ff0b140507b5ef56d7f92bd
Monika Lalwani <146726090+Monika27052004@users.noreply.github.com>
2025-01-08 10:36:23 +0530

    Update useGetAllJobs.jsx

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,11 +1,58 @@
+// import { setAllJobs } from '@/redux/jobSlice'
+// import { JOB_API_END_POINT } from '@/utils/constant'
+// import axios from 'axios'
+// import { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+
+
+
+
+// const useGetAllJobs = () => {
+//     const dispatch = useDispatch();
+//     const { searchedQuery } = useSelector(store => store.job);
+
+//     useEffect(() => {
+//         const fetchAllJobs = async () => {
+//             try {
+//                 const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, { withCredentials: true });
+//                 if (res.data.success) {
+//                     dispatch(setAllJobs(res.data.jobs));
+//                 }
+//             } catch (error) {
+//                 console.error("Error fetching jobs:", error);
+//             }
+//         };
+//         fetchAllJobs();
+//     }, [dispatch, searchedQuery]);
+// };
+
+// export default useGetAllJobs;
+
+
+// const useGetAllJobs = () => {
+//     const dispatch = useDispatch();
+//     const {searchedQuery} = useSelector(store=>store.job);
+//     useEffect(()=>{
+//         const fetchAllJobs = async () => {
+//             try {
+//                 const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
+//                 if(res.data.success){
+//                     dispatch(setAllJobs(res.data.jobs));
+//                 }
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         }
+//         fetchAllJobs();
+//     },[])
+// }
+
+// export default useGetAllJobs
 import { setAllJobs } from '@/redux/jobSlice'
 import { JOB_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
-
-
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
@@ -22,29 +69,13 @@ const useGetAllJobs = () => {
                 console.error("Error fetching jobs:", error);
             }
         };
-        fetchAllJobs();
-    }, [dispatch, searchedQuery]);
+        
+        if (searchedQuery) {
+            fetchAllJobs(); // Only fetch jobs if searchedQuery is available
+        }
+    }, [dispatch, searchedQuery]); // Ensure effect runs when `searchedQuery` changes
+
 };
 
 export default useGetAllJobs;
 
-
-const useGetAllJobs = () => {
-    const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(store=>store.job);
-    useEffect(()=>{
-        const fetchAllJobs = async () => {
-            try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
-                if(res.data.success){
-                    dispatch(setAllJobs(res.data.jobs));
-                }
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchAllJobs();
-    },[])
-}
-
-export default useGetAllJobs
