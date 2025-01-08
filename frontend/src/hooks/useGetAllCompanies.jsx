448ae041e565b600444387d5f6d1c61d789df879
Monika Lalwani <146726090+Monika27052004@users.noreply.github.com>
2025-01-08 10:38:00 +0530

    Update useGetAllCompanies.jsx

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -4,10 +4,10 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { setCompanies } from '@/redux/companySlice';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+// import { setCompanies } from '@/redux/companySlice';
+// import axios from 'axios';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 
 
 
